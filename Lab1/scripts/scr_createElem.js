016a1858971a6b0b72bfa598e0a0e4d23d9dbb21
Lukasz <rlukasz1996@gmail.com>
2018-10-23 21:48:27 +0200

    Lab2 - Web Site v 2.0

---
+++ Lab1/scripts/scr_createElem.js
@@ -0,0 +1,23 @@
+
+function addElement() {
+    
+    var newdiv = document.createElement("div");
+    newdiv.innerText = "Nowy div";
+    newdiv.classList.add("newDiv");
+    newdiv.style.setProperty("background-color", "#e1c00b");
+    newdiv.style.setProperty("height", "75px");
+    newdiv.style.setProperty("width", "100px");
+    newdiv.style.setProperty("text-align", "center");
+    newdiv.style.setProperty("margin-left", "50%");
+    document.body.appendChild(newdiv);
+
+
+    var btn = document.createElement("BUTTON");
+    btn.innerText = "Nowy Przycisk";
+    btn.style.setProperty("height", "50px");
+    btn.style.setProperty("width", "100px");
+    btn.style.setProperty("text-align", "center");
+    btn.style.setProperty("margin-left", "50%");
+    document.body.appendChild(btn);
+
+}
